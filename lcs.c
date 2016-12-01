214a9e5dc27e9ae7dbf622c90531284fba61cc81
linus_linux <1210115085@qq.com>
2016-12-01 10:06:16 +0800

    lcs.c modified and finished 2016-12-01 10:06:15

--- lcs.c
+++ lcs.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int max(int a, int b) {
     if (a > b) {
@@ -6,48 +7,102 @@ int max(int a, int b) {
     }
     return b;
 }
+/**
+ * 生成一个二维数组
+ * */
+int **create2DArray(int m, int n) {
+    int **p = (int **) malloc(sizeof(int*) * m);
+    if (p == NULL) {
+        printf("memory allocation failed!\n");
+        exit(0);
+    }
+    int i = 0;
+    for (; i < m; ++i) {
+        p[i] = (int *) malloc(sizeof(int) * n);
+        if (p[i] == NULL) {
+            printf("memory allocation failed!\n");
+            exit(0);
+        }
+    }
+    return p;
+}
+/**
+ * 将数组清0
+ *
+ * */
+void setZero2DArray(int **arr, int m, int n) {
+    int i, j;
+    for(i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            arr[i][j] = 0;
+        }
+    }
+}
+void print2DArray(int **arr, int m, int n) {
+    int i, j;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+/**
+ * 输出一条最长公共子序列
+ * */
+void printLCS(char *arr1, int **b, int i, int j) {
+    if (i == 0 || j == 0) {
+        return;
+    }
+    if (b[i][j] == 1) {
+        printLCS(arr1, b, i - 1, j - 1);
+        printf("%c ", arr1[i - 1]);
+    } else if (b[i][j] == 2) {
+        printLCS(arr1, b, i - 1, j);
+    } else if (b[i][j] == 3){
+        printLCS(arr1, b, i, j - 1);
+    }
+}
 
-
-
-void lcs(char *arr1, int n, char *arr2, int m, int L[][7]) {
+void lcs(char *arr1, int m, char *arr2, int n) {
+    // 保存i，j最大公共子序列长度
+    int **c = create2DArray(m + 1, n + 1);
+    // 简化最优解的构造
+    int **b = create2DArray(m + 1, n + 1);
     int i = 0;
     while (i <= n) {
-        L[i][0] = 0;
+        c[i][0] = 0;
         ++i;
     }
     int j = 0;
     while(j <= m) {
-        L[0][j++] = 0;
+        c[0][j++] = 0;
     }
-    for (i = 1; i <= n; ++i) {
-        for (j = 1; j <= m; ++j) {
+    for (i = 1; i <= m; ++i) {
+        for (j = 1; j <= n; ++j) {
             if (arr1[i - 1] == arr2[j - 1]) {
-                L[i][j] = L[i - 1][j - 1] + 1;
+                c[i][j] = c[i - 1][j - 1] + 1;
+                b[i][j] = 1;
             } else {
-                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+                if (c[i - 1][j] > c[i][j - 1]) {
+                    c[i][j] = c[i - 1][j];
+                    b[i][j] = 2;
+                } else {
+                    c[i][j] = c[i][j - 1];
+                    b[i][j] = 3;
+                }
             }
         }
     }
+    print2DArray(c, n + 1, n + 1);
+    print2DArray(b, n + 1, n + 1);
+    printLCS(arr1, b, m, n);
 }
 
-void printArray(int arr[][7], int n) {
-    int i = 0;
-    while (i < n) {
-        int j = 0;
-        while (j < 7) {
-            printf("%2d", arr[i][j++]);
-        }
-        ++i;
-        printf("\n");
-    }
-}
 
 int main() {
     char arr1[7] = {'a', 'b', 'c', 'b', 'd', 'a', 'b'};
     char arr2[6] = {'b', 'd', 'c', 'a', 'b', 'a'};
-    int L[8][7];
-    lcs(arr1, 7, arr2, 6, L);
-    printf("%d \n", L[7][6]);
-    printArray(L, 8);
+    lcs(arr1, 7, arr2, 6);
     return 0;
 }
